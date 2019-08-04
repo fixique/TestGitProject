68a284b9e670a95639dc75f3bb777cd8e5699887
Krupenko Validislav <dev.fixique@gmail.com>
2019-08-04 20:05:33 +0300

    Добавлен тестовый код, который используется в презентации

---
+++ TestGitProject/TestGitProject/TestHello.h
@@ -0,0 +1,17 @@
+//
+//  TestHello.h
+//  TestGitProject
+//
+//  Created by Vladislav Krupenko on 04/08/2019.
+//  Copyright © 2019 Surf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestHello : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestGitProject/TestGitProject/TestHello.m
@@ -0,0 +1,13 @@
+//
+//  TestHello.m
+//  TestGitProject
+//
+//  Created by Vladislav Krupenko on 04/08/2019.
+//  Copyright © 2019 Surf. All rights reserved.
+//
+
+#import "TestHello.h"
+
+@implementation TestHello
+
+@end
